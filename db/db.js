1d632943ab2d2e8a453473c40644552158b584ce
Maja Thunberg <majaneko@gmail.com>
2021-11-09 17:05:42 +0100

    Setup basic db communication

---
+++ db/db.js
@@ -0,0 +1,97 @@
+class Db {
+  constructor(collection) {
+    this.collection = collection
+  }
+  async find(query = [], populate = '') {
+    try {
+      const docs = await this.collection.find(query).populate(populate)
+      return docs
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async findById(id, populate = '') {
+    try {
+      const doc = await this.collection.find(id).populate(populate)
+      return doc
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async findOne(query = [], populate = '') {
+    try {
+      const doc = await this.collection.find(query).populate(populate)
+      return doc
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async create(data) {
+    try {
+      const doc = await new this.collection(data).save()
+      return doc
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async findByIdAndUpdate(id) {
+    try {
+      const doc = await this.collection.findByIdAndUpdate(id)
+      return doc
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async findOneAndUpdate(query = []) {
+    try {
+      const doc = await this.collection.findOneAndUpdate(query)
+      return doc
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async updateMany(query = []) {
+    try {
+      const docs = await this.collection.updateMany(query)
+      return docs
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async findByIdAndDelete(id) {
+    try {
+      const doc = await this.collection.updateMany(id)
+      return doc
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async findOneAndDelete(query = []) {
+    try {
+      const doc = await this.collection.findOneAndDelete(query)
+      return doc
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async deleteMany(query = []) {
+    try {
+      const docs = await this.collection.deleteMany(query)
+      return docs
+    } catch (error) {
+      throw error
+    }
+  }
+  
+}
+
+module.exports = Db
