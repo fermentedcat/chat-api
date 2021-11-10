03f341d2a8d54885eb740feb5de37417653a67af
Maja Thunberg <majaneko@gmail.com>
2021-11-10 13:50:38 +0100

    Complete chat service

--- services/chat.service.js
+++ services/chat.service.js
@@ -1,15 +1,89 @@
-const Db = require('../db/db');
-const Chat = require('../models/Chat');
+const Db = require('../db/db')
+const Chat = require('../models/Chat')
+const MessageService = require('./message.service')
+const SubscriptionService = require('./subscription.service')
 
 class ChatService {
-  // Add param db for testability
-  constructor(db = new Db(Chat)) {
-    this.db = db
+  constructor() {
+    this.db = new Db(Chat)
+    this.messageService = new MessageService()
+    this.subscriptionService = new SubscriptionService(this)
   }
-  async findAll(user) {
+  async findAll() {
     try {
-      const chats = await this.db.find()
+      const populate = 'creator'
+      const chats = await this.db.find({}, populate)
       return chats
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async findAllUserChats(user) {
+    try {
+      const { userId } = user
+      const subscriptions = await this.subscriptionService.findAllByUserId(
+        userId
+      )
+      return subscriptions
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async findById(chatId) {
+    try {
+      const chat = await this.db.findById(chatId, 'creator')
+      return chat
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async createNew(user, chatData) {
+    try {
+      const { userId } = user
+      const data = {
+        creator: userId,
+        title: chatData.title,
+        private: chatData.private
+      }
+      const chat = await this.db.create(data)
+      await this.subscriptionService.createNew(chat._id, userId)
+      return chat
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async update(chatId, user, chatData) {
+    try {
+      const { userId, role } = user
+      const chat = await this.db.findById(chatId)
+
+      if (role !== 'admin' && userId != chat.creator) {
+        throw new Error('Not authorized')
+      }
+
+      const updated = await this.db.findByIdAndUpdate(chatId, chatData)
+      return updated
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async delete(chatId, user) {
+    try {
+      const { userId, role } = user
+      const chat = await this.db.findById(chatId)
+
+      if (role !== 'admin' && userId != chat.creator) {
+        throw new Error('Not authorized')
+      }
+      const deleted = await this.db.findByIdAndDelete(chatId)
+      await this.messageService.deleteAllFromChat(chatId)
+      await this.subscriptionService.deleteAllChatSubscriptions(chatId)
+      return deleted
     } catch (error) {
       throw error
     }
