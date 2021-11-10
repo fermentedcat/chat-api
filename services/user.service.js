5712f3397a958b3882fd680149200eb5ae3402b6
Maja Thunberg <majaneko@gmail.com>
2021-11-10 13:51:49 +0100

    Complete user service

--- services/user.service.js
+++ services/user.service.js
@@ -1,24 +1,65 @@
 const Db = require('../db/db');
 const User = require('../models/User');
+const SubscriptionService = require('./subscription.service')
 
 class UserService {
-  constructor(db = new Db(User)) {
-    this.db = db
+  constructor() {
+    this.db = new Db(User)
+    this.subscriptionService = new SubscriptionService()
   }
   async findAll() {
     try {
-      const user = await this.db.find()
+      const users = await this.db.find()
+      return users
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async findById(userId) {
+    try {
+      const user = await this.db.findById(userId)
       return user
     } catch (error) {
       throw error
     }
   }
 
-  async createNew() {
-    const data = {}
+  async createNew(userData) {
     try {
-      const user = await this.db.create(data)
+      const user = await this.db.create(userData)
       return user
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async update(user, userId, userData) {
+    try {
+      const isAuthorized = user.userId === userId || user.role === 'admin'
+
+      if (!isAuthorized) {
+        throw new Error('Not authorized.')
+      }
+
+      const newUser = await this.db.findByIdAndUpdate(userId, userData)
+      return newUser
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async delete(user, userId) {
+    try {
+      const isAuthorized = user.userId === userId || user.role === 'admin'
+
+      if (!isAuthorized) {
+        throw new Error('Not authorized.')
+      }
+
+      const deleted = await this.db.findByIdAndDelete(userId)
+      await this.subscriptionService.deleteAllUserSubscriptions(userId)
+      return deleted
     } catch (error) {
       throw error
     }
