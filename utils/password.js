abc303b638a24a5f9cd6f5bbe2f51fcef2e9eb64
Maja Thunberg <majaneko@gmail.com>
2021-11-19 14:37:14 +0100

    Add password hash and match functions

---
+++ utils/password.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcrypt')
+
+exports.validate = (password) => {
+  const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/
+  return password.match(regex)
+}
+
+exports.hash = async (password) => {
+  try {
+    const saltRounds = parseInt(process.env.SALT_ROUNDS, 10)
+    return await bcrypt.hash(password, saltRounds)
+  } catch (error) {
+    throw error
+  }
+}
+
+exports.match = async (password, hash) => {
+  try {
+    return await bcrypt.match(password, hash)
+  } catch (error) {
+    throw error
+  }
+}
