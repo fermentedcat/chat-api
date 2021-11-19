270904191e2c8f967b323d2d620af17af60cb1db
Maja Thunberg <majaneko@gmail.com>
2021-11-19 14:38:29 +0100

    Remove cors config for development

--- config/app.js
+++ config/app.js
@@ -2,10 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors({
-  origin: process.env.CLIENT_URL
-}))
 
 module.exports = app
