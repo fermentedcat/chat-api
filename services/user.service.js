f4cb4ad5bb10c5207ded889b2bd3f49d69c49c95
Maja Thunberg <majaneko@gmail.com>
2021-11-19 14:43:13 +0100

    Use password and token functions

--- services/user.service.js
+++ services/user.service.js
@@ -1,5 +1,7 @@
-const Db = require('../db/db');
-const User = require('../models/User');
+const Db = require('../db/db')
+const User = require('../models/User')
+const password = require('../utils/password')
+const { create: createToken } = require('../utils/token')
 const SubscriptionService = require('./subscription.service')
 
 class UserService {
@@ -25,10 +27,42 @@ class UserService {
     }
   }
 
+  async findByUsername(username) {
+    try {
+      const query = { username: username }
+      const user = await this.db.findOne(query)
+      return user
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async login(userData) {
+    try {
+      const { email, password } = userData
+
+      const query = { email: email }
+      const user = await this.db.findOne(query)
+
+      const hash = user.password
+      await password.match(password, hash)
+
+      const token = createToken(user)
+      return token
+    } catch (error) {
+      throw error
+    }
+  }
+
   async createNew(userData) {
     try {
-      const user = await this.db.create(userData)
-      return user
+      const hashedData = {
+        ...userData,
+        password: await password.hash(userData.password),
+      }
+      const user = await this.db.create(hashedData)
+      const token = createToken(user)
+      return token
     } catch (error) {
       throw error
     }
