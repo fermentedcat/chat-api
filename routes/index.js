3b21ff9924e23a1862048c2b232e989d6955c3f6
Maja Thunberg <majaneko@gmail.com>
2021-11-24 11:35:49 +0100

    Setup sse for messages

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const chatRouter = require('./chat')
 const messageRouter = require('./message')
 const userRouter = require('./user')
 const subscriptionRouter = require('./subscription')
+const sseRouter = require('./sse')
 
 router.get('/', (req, res) =>  res.send('Please use /api/'))
 
@@ -12,5 +13,6 @@ router.use('/api/chat/', chatRouter)
 router.use('/api/message/', messageRouter)
 router.use('/api/user/', userRouter)
 router.use('/api/subscription/', subscriptionRouter)
+router.use('/api/sse/', sseRouter)
 
 module.exports = router
