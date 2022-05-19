dd440ba9a86980360636bec7a869871b273600c5
Maja Thunberg <majaneko@gmail.com>
2022-05-19 21:20:16 +0200

    Update cors config

--- config/app.js
+++ config/app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-app.use(cors())
+app.use(cors({
+  origin: process.env.CLIENT_URL
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
